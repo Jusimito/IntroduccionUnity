2f5b88283add04bc18de09c84b103850fc8c6ed7
Diego Sanchez <diego@crema.gg>
2024-07-07 10:47:04 +0200

    commit preparando el tema 2 de la unidad 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestComponent : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 0, 0);
+        
     }
 
     // Update is called once per frame
