cc65edf6e4285347ca4afcebd6f6a9d78dc4837e
Diego Sanchez <43349836+Jusimito@users.noreply.github.com>
2024-07-09 17:06:56 +0200

    Update MovingObject.cs

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -12,21 +12,31 @@ public class MovingObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //inicializamos nuestras variables
         startPosition = transform.position;
         finishPosition = startPosition + moveOffset;
+        //asi hacemos que el primer movimiento sea hacia el final de la trayectoria
         targetPosition = finishPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //guardamos la posición actual del objeto
         Vector2 currentPosition = transform.position;
+        //calculamos la nueva posicion siguiendo el vector de movimiento
         currentPosition += (targetPosition - currentPosition).normalized * Time.deltaTime;
+        //aplicamos la traslación
         transform.position = currentPosition;
+
+
+        //si esta muy cerca del final de la trayectoria hacemos que vaya hacia el principio
         if((currentPosition - finishPosition).magnitude < 0.01f)
         {
             targetPosition = startPosition;
-        } else if ((currentPosition - startPosition).magnitude < 0.01f)
+        }
+        //si esta muy cerca del inicio de la trayectoria hacemos que vaya hacia el final
+        else if ((currentPosition - startPosition).magnitude < 0.01f)
         {
             targetPosition = finishPosition;
         }
